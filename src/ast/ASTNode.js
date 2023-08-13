31668eb7b4bf8a956ba1ba5963a834a557fb2cc3
Davyd Kohut <expexes@gmail.com>
2023-08-14 02:17:15 +0300

    0.11.6

--- src/ast/ASTNode.js
+++ src/ast/ASTNode.js
@@ -3,17 +3,22 @@ class ASTNode {
      * @param {ParserRuleContext} ctx
      */
     constructor(ctx) {
-        this.context = {
-            start: {
-                line: ctx.start.line,
-                column: ctx.start.column,
-            },
-            stop: {
-                line: ctx.stop.line,
-                column: ctx.stop.column,
-            },
-            fileinfo: ctx.parser._input.tokenSource._input._mavka_fileinfo,
-        };
+        if (ctx.__ast_node__) {
+            this.context = ctx;
+        } else {
+            this.context = {
+                __ast_node__: true,
+                start: {
+                    line: ctx.start.line,
+                    column: ctx.start.column,
+                },
+                stop: {
+                    line: ctx.stop.line,
+                    column: ctx.stop.column,
+                },
+                fileinfo: ctx.parser._input.tokenSource._input._mavka_fileinfo,
+            };
+        }
     }
 }
 
